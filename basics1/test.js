de6335bb32bd632423b20a8515d3285e7d1cbaaf
Punit160 <115865222+Punit160@users.noreply.github.com>
2024-04-25 05:42:44 +0000

    datatypes

--- basics1/test.js
+++ basics1/test.js
@@ -1,1 +1,12 @@
 console.log("Punit")
+
+const accountId = 1234
+let email = 'punit123@gmail.com'
+var section = 'first'
+city = 'Mumbai'
+
+
+email = 'punit234@gmail.com'
+
+
+console.table([accountId, email, section,city])
---
+++ basics2/index.js
@@ -0,0 +1,8 @@
+"use strict"
+
+console.log(3+3)
+console.log("Punit")
+
+
+console.log(typeof "Punit")
+console.log(typeof null)
